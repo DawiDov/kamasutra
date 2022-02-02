38bb6fd44c603f1baafbe6cf8ce319a37cf696b1
Артур Давидов <art@mac.local>
2022-02-02 22:28:22 +0300

    ras

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,78 +1,98 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import Users from "./Users";
-import {CAFollowed, CASetCurrentPage, CASetUsers, CAUnfollowed, CASetTotalUsersCount, CASetIsFetchingCount} from "../mobx/users-reducer";
+import {
+  CAFollowed,
+  CASetCurrentPage,
+  CASetUsers,
+  CAUnfollowed,
+  CASetTotalUsersCount,
+  CASetIsFetchingCount,
+} from "../mobx/users-reducer";
 import * as axios from "axios";
 import Preloader from "../common/preloader/Preloader";
 
 class UsersContainer extends React.Component {
+  // componentDidMount() {
+  //     this.props.setIsFetchingCount(!this.props.isFetching)
+  //     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+  //         this.props.setIsFetchingCount(!this.props.isFetching)
+  //         this.props.setUsers(response.data.items);
+  //         this.props.setTotalUsersCount(response.data.totalCount);
+  //     });
+  // }
 
-    componentDidMount() {
-        this.props.setIsFetchingCount(!this.props.isFetching)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setIsFetchingCount(!this.props.isFetching)
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
+  // onPageChanged = (pageNumber) => {
+  //     this.props.setIsFetchingCount(!this.props.isFetching)
+  //     this.props.setCurrentPage(pageNumber);
+  //     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+  //         this.props.setIsFetchingCount(!this.props.isFetching)
+  //         this.props.setUsers(response.data.items);
+  //     });
+  // }
+  componentDidMount() {
+    for (let i = 0; i >= 100; i++) {
+      this.props.setIsFetchingCount(!this.props.isFetching);
+      axios
+        .get(
+          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+        )
+        .then((response) => {
+          this.props.setIsFetchingCount(!this.props.isFetching);
+          this.props.setUsers(response.data.items);
+          this.props.setTotalUsersCount(response.data.totalCount);
         });
     }
-
-    onPageChanged = (pageNumber) => {
-        this.props.setIsFetchingCount(!this.props.isFetching)
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setIsFetchingCount(!this.props.isFetching)
-            this.props.setUsers(response.data.items);
-        });
-    }
-
-    render() {
-        return <>
-            { this.props.isFetching ? <Preloader/> : null }
-            <Users onPageChanged={ this.onPageChanged }
-                          users={this.props.users}
-                          totalUsersCount={this.props.totalUsersCount}
-                          pageSize={this.props.pageSize}
-                          currentPage={this.props.currentPage}
-                          removeFromFriends={this.props.removeFromFriends}
-                          addAsFriends={this.props.addAsFriends}
-                        />
-            </>
-    }
+  }
+  render() {
+    return (
+      <>
+        {this.props.isFetching ? <Preloader /> : null}
+        <Users
+          onPageChanged={this.onPageChanged}
+          users={this.props.users}
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          removeFromFriends={this.props.removeFromFriends}
+          addAsFriends={this.props.addAsFriends}
+        />
+      </>
+    );
+  }
 }
-
 
 let mapStateToProps = (state) => {
-    return {
-        users: state.usersData.users,
-        pageSize: state.usersData.pageSize,
-        totalUsersCount: state.usersData.totalUsersCount,
-        currentPage: state.usersData.currentPage,
-        isFetching: state.usersData.isFetching
-    }
-}
+  return {
+    users: state.usersData.users,
+    pageSize: state.usersData.pageSize,
+    totalUsersCount: state.usersData.totalUsersCount,
+    currentPage: state.usersData.currentPage,
+    isFetching: state.usersData.isFetching,
+  };
+};
 
 let mapDispatchToProps = (dispatch) => {
-    return {
-        addAsFriends: (userId) => {
-            dispatch(CAFollowed(userId))
-        },
-        removeFromFriends: (userId) => {
-            dispatch(CAUnfollowed(userId))
-        },
-        setUsers: (users) => {
-            dispatch(CASetUsers(users))
-        },
-        setCurrentPage: (page) => {
-            dispatch(CASetCurrentPage(page))
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(CASetTotalUsersCount(totalCount))
-        },
-        setIsFetchingCount: (isFetching) => {
-            dispatch(CASetIsFetchingCount(isFetching))
-        }
-    }
-}
+  return {
+    addAsFriends: (userId) => {
+      dispatch(CAFollowed(userId));
+    },
+    removeFromFriends: (userId) => {
+      dispatch(CAUnfollowed(userId));
+    },
+    setUsers: (users) => {
+      dispatch(CASetUsers(users));
+    },
+    setCurrentPage: (page) => {
+      dispatch(CASetCurrentPage(page));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(CASetTotalUsersCount(totalCount));
+    },
+    setIsFetchingCount: (isFetching) => {
+      dispatch(CASetIsFetchingCount(isFetching));
+    },
+  };
+};
 
-
-export default connect (mapStateToProps, mapDispatchToProps)(UsersContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
--- src/components/mobx/users-reducer.js
+++ src/components/mobx/users-reducer.js
@@ -1,78 +1,75 @@
-const FOLLOWED = 'FOLLOWED';
-const UNFOLLOWED = 'UNFOLLOWED';
-const SET_USERS = 'SET-USERS';
-const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
-const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT'
-const TOGGLE_IS_FETCHING = 'TOGGLE-IS-FETCHING'
+const FOLLOWED = "FOLLOWED";
+const UNFOLLOWED = "UNFOLLOWED";
+const SET_USERS = "SET-USERS";
+const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
+const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT";
+const TOGGLE_IS_FETCHING = "TOGGLE-IS-FETCHING";
 
 const initialState = {
-    users: [],
-    pageSize: 15,
-    totalUsersCount: 0,
-    currentPage: 1,
-    isFetching: false
-}
+  users: [],
+  pageSize: 15,
+  totalUsersCount: 0,
+  currentPage: 1,
+  isFetching: false,
+};
 
 const usersReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case FOLLOWED:
-            return {
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return { ...u, followed: true }
-                    }
-                    return u;
-                })
-            }
-        case TOGGLE_IS_FETCHING:
-            return {
-                ...state,
-                isFetching: action.isFetching
-            }
-        case UNFOLLOWED:
-            return{
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return { ...u, followed: false }
-                    }
-                    return u;
-                })
-            }
-        case SET_TOTAL_USERS_COUNT:
-            return { ...state, totalUsersCount: action.totalCount }
+  switch (action.type) {
+    case FOLLOWED:
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: true };
+          }
+          return u;
+        }),
+      };
+    case TOGGLE_IS_FETCHING:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
+    case UNFOLLOWED:
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: false };
+          }
+          return u;
+        }),
+      };
+    case SET_TOTAL_USERS_COUNT:
+      return { ...state, totalUsersCount: action.totalCount };
 
-        case SET_CURRENT_PAGE:
-            return { ...state, currentPage: action.currentPage }
+    case SET_CURRENT_PAGE:
+      return { ...state, currentPage: action.currentPage };
 
-        case SET_USERS:
-            return { ...state, users: action.users }
-        default:
-            return state
-
-
-    }
-}
-
+    case SET_USERS:
+      return { ...state, users: action.users };
+    default:
+      return state;
+  }
+};
 
 export let CAFollowed = (id) => {
-    return { type: FOLLOWED, userId: id }
+  return { type: FOLLOWED, userId: id };
 };
 export let CAUnfollowed = (id) => {
-    return { type: UNFOLLOWED, userId: id }
+  return { type: UNFOLLOWED, userId: id };
 };
 export let CASetUsers = (users) => {
-    return { type: SET_USERS, users: users }
+  return { type: SET_USERS, users: users };
 };
 export let CASetCurrentPage = (page) => {
-    return { type: SET_CURRENT_PAGE, currentPage: page }
+  return { type: SET_CURRENT_PAGE, currentPage: page };
 };
 export let CASetTotalUsersCount = (totalCount) => {
-    return { type: SET_TOTAL_USERS_COUNT, totalCount: totalCount }
+  return { type: SET_TOTAL_USERS_COUNT, totalCount: totalCount };
 };
 export let CASetIsFetchingCount = (isFetching) => {
-    return {type: TOGGLE_IS_FETCHING, isFetching}
+  return { type: TOGGLE_IS_FETCHING, isFetching };
 };
 
 export default usersReducer;
